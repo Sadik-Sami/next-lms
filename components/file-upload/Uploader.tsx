973169993f5b00fbd8e4f64592ee29f1ec58666a
Sadik-Sami <miasimanto@gmail.com>
2025-08-25 04:23:15 +0600

    file upload created, debugging 403 on Upload

--- components/file-upload/Uploader.tsx
+++ components/file-upload/Uploader.tsx
@@ -1,14 +1,161 @@
 'use client';
-import { useCallback } from 'react';
-import { useDropzone } from 'react-dropzone';
+import { useCallback, useState } from 'react';
+import { FileRejection, useDropzone } from 'react-dropzone';
 import { Card, CardContent } from '../ui/card';
 import { cn } from '@/lib/utils';
+import { RenderEmptyState, RenderErrorState } from './RenderState';
+import { toast } from 'sonner';
+import { v4 as uuidv4 } from 'uuid';
+
+interface UploaderState {
+	id: string | null;
+	file: File | null;
+	uploading: boolean;
+	progress: number;
+	key?: string;
+	isDeleting: boolean;
+	error: boolean;
+	objectUrl?: string;
+	fileType: 'image' | 'video';
+}
 
 export default function Uploader() {
+	const [fileState, setFileState] = useState<UploaderState>({
+		error: false,
+		file: null,
+		id: null,
+		uploading: false,
+		progress: 0,
+		isDeleting: false,
+		fileType: 'image',
+	});
+
+	async function uploadFile(file: File) {
+		setFileState((prev) => ({
+			...prev,
+			uploading: true,
+			progress: 0,
+		}));
+
+		try {
+			const presignedResponse = await fetch('/api/s3/upload', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					fileName: file.name,
+					contentType: file.type,
+					size: file.size,
+					isImage: true,
+				}),
+			});
+			if (!presignedResponse.ok) {
+				toast.error('Failed to get presigned URL');
+				setFileState((prev) => ({
+					...prev,
+					uploading: false,
+					progress: 0,
+					error: true,
+				}));
+				return;
+			}
+
+			const { presignedUrl, key } = await presignedResponse.json();
+
+			await new Promise<void>((resolve, reject) => {
+				const xhr = new XMLHttpRequest();
+				xhr.upload.onprogress = (event) => {
+					if (event.lengthComputable) {
+						const percentageCompleted = (event.loaded / event.total) * 100;
+						setFileState((prev) => ({
+							...prev,
+							progress: Math.round(percentageCompleted),
+						}));
+					}
+				};
+				xhr.onload = () => {
+					if (xhr.status === 200 || xhr.status === 204) {
+						setFileState((prev) => ({
+							...prev,
+							progress: 100,
+							uploading: false,
+							key: key,
+						}));
+						toast.success('File upload successfully');
+						resolve();
+					} else {
+						reject(new Error('Upload Failed'));
+					}
+				};
+				xhr.onerror = () => {
+					reject(new Error('Upload Failed'));
+				};
+				xhr.open('PUT', presignedUrl);
+				xhr.setRequestHeader('Content-Type', file.type);
+				xhr.send(file);
+			});
+		} catch {
+			toast.error('Something went wrong');
+			setFileState((prev) => ({
+				...prev,
+				progress: 0,
+				error: true,
+				uploading: false,
+			}));
+		}
+	}
+
 	const onDrop = useCallback((acceptedFiles: File[]) => {
-		console.log(acceptedFiles);
+		if (acceptedFiles.length > 0) {
+			const file = acceptedFiles[0];
+			setFileState({
+				file: file,
+				uploading: false,
+				progress: 0,
+				objectUrl: URL.createObjectURL(file),
+				error: false,
+				id: uuidv4(),
+				isDeleting: false,
+				fileType: 'image',
+			});
+			uploadFile(file);
+		}
 	}, []);
-	const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+	function rejectedFiles(fileRejection: FileRejection[]) {
+		if (fileRejection.length) {
+			const tooManyFiles = fileRejection.find((rejection) => rejection.errors[0].code === 'too-many-files');
+			const fileSizeTooBig = fileRejection.find((rejection) => rejection.errors[0].code === 'file-too-large');
+			if (tooManyFiles) {
+				return toast.error('Too many files selected, max is 1');
+			}
+			if (fileSizeTooBig) {
+				return toast.error('File Size exceeds the limit');
+			}
+		}
+	}
+
+	function renderContent() {
+		if (fileState.uploading) {
+			return <h1>uploading</h1>;
+		}
+		if (fileState.error) {
+			return <RenderErrorState />;
+		}
+		if (fileState.objectUrl) {
+			return <h1>Uploaded File</h1>;
+		}
+
+		return <RenderEmptyState isDragActive={isDragActive} />;
+	}
+
+	const { getRootProps, getInputProps, isDragActive } = useDropzone({
+		onDrop,
+		accept: { 'image/*': [] },
+		maxFiles: 1,
+		multiple: false,
+		maxSize: 5 * 1024 * 1024, // 5mb file size
+		onDropRejected: rejectedFiles,
+	});
 	return (
 		<Card
 			{...getRootProps()}
@@ -18,7 +165,7 @@ export default function Uploader() {
 			)}>
 			<CardContent className='flex items-center justify-center h-full w-full p-4'>
 				<input {...getInputProps()} />
-				<p>Dummy Code</p>
+				{renderContent()}
 			</CardContent>
 		</Card>
 	);
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,10 @@
+import 'server-only';
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { prisma } from './db';
 import { env } from './env';
 import { emailOTP } from 'better-auth/plugins';
+import 'server-only';
 import { resend } from './resend';
 
 export const auth = betterAuth({
@@ -18,7 +20,7 @@ export const auth = betterAuth({
 	plugins: [
 		emailOTP({
 			async sendVerificationOTP({ email, otp }) {
-				const { data, error } = await resend.emails.send({
+				await resend.emails.send({
 					from: 'Next-LMS <onboarding@resend.dev>',
 					to: [email],
 					subject: 'NEXTLMS: Email Verification',
